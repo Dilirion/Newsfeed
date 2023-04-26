7c64b355058abfcac9291a2c5037ea337002b218
Artur Krom <artur.krom@dnateam.ru>
2023-04-27 02:12:22 +0300

    add firebase

---
+++ src/api.ts
@@ -0,0 +1,145 @@
+import { initializeApp } from 'firebase/app';
+import {
+  collection,
+  getDocs,
+  getFirestore,
+  addDoc,
+  getDoc,
+  doc,
+  updateDoc,
+  deleteDoc,
+  query,
+  orderBy,
+  limit,
+} from 'firebase/firestore';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { IPartnerArticle } from './Types';
+
+export const initializeAPI = () => {
+  initializeApp({
+    apiKey: 'AIzaSyDbnbrtLn7gyZU11tSpjvdN6F81aNJuYnQ',
+    authDomain: 'krom-karpov-news.firebaseapp.com',
+    projectId: 'krom-karpov-news',
+    storageBucket: 'krom-karpov-news.appspot.com',
+    messagingSenderId: '862108373621',
+    appId: '1:862108373621:web:ba9132dc3128ff6a62de3c',
+  });
+
+  getFirestore();
+  getStorage();
+};
+
+const partnersPostsCollection = 'partners-posts';
+
+export const getPartnersArticles = async (): Promise<IPartnerArticle[]> => {
+  const db = getFirestore();
+  const querySnapshot = await getDocs(collection(db, partnersPostsCollection));
+  const articles: IPartnerArticle[] = [];
+
+  try {
+    querySnapshot.forEach((doc) => {
+      const data = doc.data() as Omit<IPartnerArticle, 'id'>;
+
+      articles.push({
+        id: doc.id,
+        ...data,
+      });
+    });
+  } catch (error) {
+    return Promise.reject(error);
+  }
+
+  return articles;
+};
+
+export const createPartnerArticle = async (data: Omit<IPartnerArticle, 'id' | 'created'>): Promise<any> => {
+  const db = getFirestore();
+
+  try {
+    await addDoc(collection(db, partnersPostsCollection), data);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+export const updatePartnerArticle = async (id: string, data: Omit<IPartnerArticle, 'id' | 'created'>): Promise<any> => {
+  const db = getFirestore();
+
+  const ref = doc(db, partnersPostsCollection, id);
+
+  try {
+    await updateDoc(ref, data);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+export const deletePartnerArticle = async (id: string): Promise<any> => {
+  const db = getFirestore();
+
+  const ref = doc(db, partnersPostsCollection, id);
+
+  try {
+    await deleteDoc(ref);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+export const getPartnerArticle = async (id: string): Promise<IPartnerArticle> => {
+  const db = getFirestore();
+  const docRef = doc(db, partnersPostsCollection, id);
+  try {
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const data = docSnap.data() as Omit<IPartnerArticle, 'id'>;
+
+      return {
+        id: docSnap.id,
+        ...data,
+      };
+    } else {
+      throw Error('Такой статьи нет');
+    }
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+export const uploadFile = async (file: File): Promise<string> => {
+  const storage = getStorage();
+  const storageRef = ref(storage, `${file.name}-${Date.now()}`);
+
+  try {
+    const shapshot = await uploadBytes(storageRef, file);
+    const url = await getDownloadURL(shapshot.ref);
+
+    return url;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+export const getMainPartnerArticle = async (): Promise<IPartnerArticle | null> => {
+  const db = getFirestore();
+  let article = null;
+
+  try {
+    const q = query(collection(db, partnersPostsCollection), orderBy('created', 'desc'), limit(1));
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      const data = doc.data() as Omit<IPartnerArticle, 'id'>;
+
+      article = {
+        id: doc.id,
+        ...data,
+      };
+    });
+  } catch (error) {
+    return Promise.reject(error);
+  }
+
+  return article;
+};
